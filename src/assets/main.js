bd532978f29b3f3b868154597bf1544551355cbf
bpetrieux <benjaminpetrieux@gmail.com>
2017-04-27 15:16:38 -0400

    check

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,32 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (setHiddenFields() == ''){
+    	setHiddenFields();
+    	validateInput();
+    }
 }
 
 //implement new functions here
+function setHiddenFields(){
+	attempt = 0;
+	let answer = Math.floor((Math.random()*4)*1000);
+	answer = answer.toString();
+
+	while (answer.length<4){
+		var zero = '0';
+		answer = zero+answer;
+	}
+	console.log(answer);
+}
+function setMessage(message){
+	$( "#answer" ).html(message);
+}
+
+function validateInput(userguess){
+	if (userguess.length=4){
+		return true
+	} else {
+		setMessage('Guesses must be exactly 4 characters long.');
+	}
+}
